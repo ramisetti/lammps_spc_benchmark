73c29872d4c7fa381e5e2bca410cafb8280f6e36
S.B.Ramisetti <ramisettisrinivas@yahoo.com>
2018-02-03 16:02:12 +0000

    script is now more generic

--- extract_speedup.sh
+++ extract_speedup.sh
@@ -1,27 +1,30 @@
-printf '#no_of_nodes time_per_100steps speed-up ideal-speed-up\n' > speedup-324k.txt 
-flag=0
-t1=0
-for i in $(ls -v run-324k-*.o*)
+arr=()
+for i in $(ls -v tmp-*-*.o*)
 do
-n=$(echo "$i" | awk -F'-' '{print $3}' | awk -F'.' '{print $1}')
-s=$(awk '/Loop time of /{print $4}' $i)
-if [ $flag -eq 0 ]; then
-t1=$s
-flag=1
-fi
-sr=$(awk -v t1=$t1 -v tN=$s 'BEGIN{print t1/tN}')
-printf '%d %f %f %f\n' $n $s $sr $n>> speedup-324k.txt
+    arr+=($(echo $i | cut -f2 -d'-'))
 done
+all="${arr[@]}"
+all=$(echo $all | tr ' ' '\n' | sort -nu)    
+cases=($all)
 
-printf '#no_of_nodes time_per_100steps\n' > speedup-1512k.txt 
-for i in $(ls -v run-1512k-*.o*)
+for cas in "${cases[@]}"
 do
-n=$(echo "$i" | awk -F'-' '{print $3}' | awk -F'.' '{print $1}')
-s=$(awk '/Loop time of /{print $4}' $i)
-if [ $flag -eq 0 ]; then
-t1=$s
-flag=1
-fi
-sr=$(awk -v t1=$t1 -v tN=$s 'BEGIN{print t1/tN}')
-printf '%d %f %f %f\n' $n $s $sr $n >> speedup-1512k.txt
-done
+    printf $cas
+    printf '#no_of_nodes time_per_100steps speed-up ideal-speed-up parallel_efficiency\n' > speedup-$cas.txt 
+    flag=0
+    t1=0
+    for i in $(ls -v tmp-$cas-*.o*)
+    do
+	n=$(echo "$i" | awk -F'-' '{print $3}' | awk -F'.' '{print $1*24}')
+	s=$(awk '/Loop time of /{print $4}' $i)
+	if [ $flag -eq 0 ]; then
+	    t1=$s
+	    flag=1
+	fi
+	printf ' %d' $n
+	sr=$(awk -v t1=$t1 -v tN=$s 'BEGIN{print 24*t1/tN}')
+	pe=$(awk -v s=$sr -v N=$n 'BEGIN{print s*100/N}')
+	printf '%d %f %f %f %f\n' $n $s $sr $n $pe>> speedup-$cas.txt
+    done
+    printf '\n' 
+done  
